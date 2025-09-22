85e9d8b9df6acaad5c762b9bf34fd0f939fd12fa
Roncode-enter <rkronakgupta@gmail.com>
2025-09-23 01:08:30 +0530

    Fix research admin.py: import admin properly, fix ModelAdmin usage, close fieldsets tuple

--- backend/apps/research/admin.py
+++ backend/apps/research/admin.py
@@ -1,10 +1,13 @@
 # Research Admin
+from django.utils.html import format_html
 from apps.research.models import Publication, ResearchProject, ResearchCategory
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
+from import_export.admin import ImportExportModelAdmin  # Assuming you use this
 
 @admin.register(ResearchCategory)
-class ResearchCategoryAdmin(ModelAdmin):
+class ResearchCategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'is_active', 'order', 'color_preview']
     list_editable = ['is_active', 'order']
 
@@ -16,7 +19,7 @@ class ResearchCategoryAdmin(ModelAdmin):
     color_preview.short_description = "Color"
 
 @admin.register(Publication)
-class PublicationAdmin(ModelAdmin, ImportExportModelAdmin):
+class PublicationAdmin(admin.ModelAdmin, ImportExportModelAdmin):
     list_display = ['title', 'publication_type', 'status', 'publication_date', 'download_count']
     list_filter = ['publication_type', 'status', 'category', 'publication_date']
     search_fields = ['title', 'abstract', 'keywords']
@@ -25,22 +28,23 @@ class PublicationAdmin(ModelAdmin, ImportExportModelAdmin):
     filter_horizontal = ['authors']
 
     fieldsets = (
-    ('Basic Information', {
-        'fields': ('title', 'abstract', 'publication_type', 'status', 'featured')
-    }),
-    ('Publication Details', {
-        'fields': ('journal_name', 'publisher', 'publication_date', 'volume', 'issue', 'pages')
-    }),
-    ('Identifiers', {
-        'fields': ('doi', 'isbn', 'issn', 'arxiv_id')
-    }),
-    ('Files and Links', {
-        'fields': ('pdf_file', 'external_url', 'cover_image')
-    }),
-    ('Categorization', {
-        'fields': ('category', 'keywords', 'tags')
-    }),
-    ('Metrics', {
-        'fields': ('download_count', 'view_count', 'citation_count')
-    }),
-)  # <-- Add this closing parenthesis
+        ('Basic Information', {
+            'fields': ('title', 'abstract', 'publication_type', 'status', 'featured')
+        }),
+        ('Publication Details', {
+            'fields': ('journal_name', 'publisher', 'publication_date', 'volume', 'issue', 'pages')
+        }),
+        ('Identifiers', {
+            'fields': ('doi', 'isbn', 'issn', 'arxiv_id')
+        }),
+        ('Files and Links', {
+            'fields': ('pdf_file', 'external_url', 'cover_image')
+        }),
+        ('Categorization', {
+            'fields': ('category', 'keywords', 'tags')
+        }),
+        ('Metrics', {
+            'fields': ('download_count', 'view_count', 'citation_count')
+        }),
+    )  # This closing parenthesis properly ends the tuple
+
